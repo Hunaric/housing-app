344df8bc6591db71e6e95047ef0ca744a0bd2ef1
Hunaric <nathanaelfangnon@gmail.com>
2024-11-28 21:25:35 +0100

    Description and details for add property modal

--- src/app/part/modal-content/add-property-modal/add-property-modal.component.ts
+++ src/app/part/modal-content/add-property-modal/add-property-modal.component.ts
@@ -30,14 +30,33 @@ export class AddPropertyModalComponent {
     
   }
 
+  minPrice = 1000;
+  minValue = 1
+  // Form 
   propertyForm = new FormGroup({
     category: new FormControl('', [Validators.required]),
     title: new FormControl('', [Validators.required]),
     description: new FormControl('', [Validators.required, Validators.minLength(10)]),
-    price: new FormControl('', [Validators.required, Validators.min(1000)]),
-    bedroom: new FormControl('', [Validators.required, Validators.min(1)]),
-    bathroom: new FormControl('', [Validators.required, Validators.min(1)]),
-    guests: new FormControl('', [Validators.required, Validators.min(1)]),
+    price: new FormControl(this.minPrice, [
+      Validators.required, 
+      Validators.min(this.minPrice),
+      Validators.pattern('^[0-9]+$'),
+    ]),
+    bedroom: new FormControl(this.minValue, [
+      Validators.required, 
+      Validators.min(this.minValue),
+      Validators.pattern('^[0-9]+$'),
+    ]),
+    bathroom: new FormControl(this.minValue, [
+      Validators.required, 
+      Validators.min(this.minValue),
+      Validators.pattern('^[0-9]+$'),
+    ]),
+    guests: new FormControl(this.minValue, [
+      Validators.required, 
+      Validators.min(this.minValue),
+      Validators.pattern('^[0-9]+$'),
+    ]),
     country: new FormControl('', [Validators.required]),
     image: new FormControl(null, [Validators.required]),
   })
@@ -69,8 +88,50 @@ export class AddPropertyModalComponent {
           valid = false
         }
         break;
+
+      case 2:
+        if(!this.propertyForm.get('title')?.value) {
+          this.errors.push('Please enter a title');
+          valid = false;
+        }
+        if(!this.propertyForm.get('description')?.value) {
+          this.errors.push('Please enter a description');
+          valid = false;
+        }
+        break;
+
+      case 3:
+        if (
+          !this.checkFieldErrors('price') || 
+          !this.checkFieldErrors('bedroom') ||
+          !this.checkFieldErrors('bathroom') ||
+          !this.checkFieldErrors('guests')
+        ) {
+          valid = false; // Si une des validations échoue, l'ensemble devient invalide
+        }
+        break;
     }
     return valid;
+  }
+
+  checkFieldErrors(fieldName: string): boolean {
+    const control = this.propertyForm.get(fieldName);
+    if (control?.invalid && control.touched) {
+      if (control.errors?.['required']) {
+        this.errors.push(`${this.capitalize(fieldName)} is required`);
+      }
+      if (control.errors?.['min']) {
+        const minValue = control.errors['min'].min;
+        this.errors.push(`${this.capitalize(fieldName)} must be at least ${minValue}`);
+      }
+      return false; // Retourner false si des erreurs sont trouvées
+    }
+    return true; // Retourner true si aucun problème n'est trouvé
+  }
+  
+
+  capitalize(fieldName: string): string {
+    return fieldName.charAt(0).toUpperCase() + fieldName.slice(1);
   }
 
   submitForm() {
