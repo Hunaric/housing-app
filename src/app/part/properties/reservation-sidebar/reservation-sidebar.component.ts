c9f94573c48c947f251d116a08afce1ec9a514d9
hunaric <nathanaelfangnon@gmail.com>
2024-12-05 21:34:48 +0100

    Start booking

--- src/app/part/properties/reservation-sidebar/reservation-sidebar.component.ts
+++ src/app/part/properties/reservation-sidebar/reservation-sidebar.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, INJECTOR, Input, signal, WritableSignal } from '@angular/core';
+import {  PropertyDetail } from '../../../interfaces/properties';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reservation-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './reservation-sidebar.component.html',
   styleUrl: './reservation-sidebar.component.css'
 })
 export class ReservationSidebarComponent {
-
+  @Input() id!: string;
+  @Input() guests!: number;
+  @Input() price_per_night!: number;
+  
+   // Fonction pour générer un tableau de 1 à `guests`
+   getGuestsArray(): number[] {
+    return Array.from({ length: this.guests }, (_, i) => i + 1);
+  }
 }
