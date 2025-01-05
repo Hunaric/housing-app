c16186ce92593ecc998eca6a1c272dc94dc04ae0
hunaric <nathanaelfangnon@gmail.com>
2025-01-06 00:19:29 +0100

    Favorite start

---
+++ src/app/favorite-button/favorite-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite-button',
+  imports: [],
+  templateUrl: './favorite-button.component.html',
+  styleUrl: './favorite-button.component.css'
+})
+export class FavoriteButtonComponent {
+
+}
