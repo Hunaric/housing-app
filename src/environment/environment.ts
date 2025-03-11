292b740526a553dd42cea50c2b16c2130bfb269a
hunaric <nathanaelfangnon@gmail.com>
2025-03-11 12:19:51 +0100

    Again

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiUrl: 'https://housing-app-backend.onrender.com',
     // apiUrl: 'http://localhost:8000',
-    WS_HOST: 'wss://housing-app-backend.onrender.com/ws/chat/'
+    WS_HOST: 'wss://housing-app-backend.onrender.com/ws/'
 }
